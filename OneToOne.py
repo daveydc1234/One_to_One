825fe7892a62bded8e16e0a59e3c667690b3428d
David Cho <daveydc1234@daveyxd1@gmail.com>
2020-03-14 17:15:20 -0400

    Code for One-to-one character mapping.

--- OneToOne.py
+++ OneToOne.py
@@ -1,29 +1,15 @@
 def OnetoOne(s1, s2):
-    my_dict = {}
-    if(len(s1)!=len(s2)):
-        print("False")
+    my_dict = {} #empty dictionary to hold one to one mapping
+    if(len(s1)!=len(s2)): #check if lengths of string are unequal
+        print("false")
         return
-    for i in range(len(s1)):
-        if s1[i] not in my_dict:
+    for i in range(len(s1)): #for every character in s1 do:
+        if s1[i] not in my_dict: #if character hasn't been seen before
             my_dict.setdefault(s1[i], s2[i])
-        else:
-            if(my_dict[s1[i]]!=s2[i]):
-                print("False")
+        else: #if character already exists in dictionary
+            if(my_dict[s1[i]]!=s2[i]): #check if key is mapping to two different values
+                print("false")
                 return
-    print("True")
-    return
+    print("true") #reach to end of code means one to one character mapping is possible
 
 
-if __name__ == "__main__":
-    str1 = "abc"
-    str2 = "bcd"
-    OnetoOne(str1, str2)
-    str1 = "foo"
-    str2 = "bar"
-    OnetoOne(str1, str2)
-    str1 = "bar"
-    str2 = "foo"
-    OnetoOne(str1, str2)
-    str1 = "123"
-    str2 = "321"
-    OnetoOne(str1, str2)
---
+++ main.py
@@ -0,0 +1,20 @@
+from OneToOne import OnetoOne
+
+if __name__ == "__main__":
+    s1 = input()
+    s2 = input()
+    OnetoOne(s1, s2)
+    """""
+    str1 = "abc"
+    str2 = "bcd"
+    OnetoOne(str1, str2)
+    str1 = "foo"
+    str2 = "bar"
+    OnetoOne(str1, str2)
+    str1 = "bar"
+    str2 = "foo"
+    OnetoOne(str1, str2)
+    str1 = "123"
+    str2 = "321"
+    OnetoOne(str1, str2)
+    """""
